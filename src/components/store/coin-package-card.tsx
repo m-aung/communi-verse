7d4cbdcbb58f5383733025a372982ed4b3afdfe4
Myo Aung <myothuwinaung1991@gmail.com>
2025-06-10 04:22:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/store/coin-package-card.tsx
+++ src/components/store/coin-package-card.tsx
@@ -1,3 +1,6 @@
+
+"use client";
+
 import Image from 'next/image';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
